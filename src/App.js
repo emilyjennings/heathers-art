dcd0fab48adca515b75601af5d0839e0daa1fca7
github username <github email address>
2019-09-14 16:03:55 -0400

    made some components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
 
-import Main from './containers/Main'
+import MainGallery from './containers/Main'
+import Gallery1 from './components/Gallery1'
+import Gallery2 from './components/Gallery2'
+import Contact from './components/Contact'
+import About from './components/About'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        < Main />
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="app">
+         <MainGallery />
+
+          <Route exact path="/" component={MainGallery} />
+          <Route exact path="/gallery1" component={Gallery1} />
+          <Route exact path="/gallery2" component={Gallery2} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/about" component={About} />
+
+        </div>
+      </BrowserRouter>
+
+    );
+  }
 }
 
 export default App;
